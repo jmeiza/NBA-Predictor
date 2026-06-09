5108e8ad006fcf58c2bb5d4001f341b4223604e6
johnmoses <jmoses1519@gmail.com>
2026-06-09 16:01:23 -0400

    First commit. Setting up the exploring data file.

---
+++ explore_data1.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+df = pd.read_csv("games.csv")
+df_teams = pd.read_csv("teams.csv")
+
+# Dropping rows where stats are missing
+df = df.dropna()
+
+print(df.shape) # (row,columns)
+# print(df.columns) # column names
+# print(df.head()) # first 5 rows
+# print(df.isnull().sum()) # missing values
+
+print(df['HOME_TEAM_WINS'].mean())
+
+print(df_teams.shape) # (row,columns)
+# print(df_teams.columns) # column names
+# print(df_teams.head()) # first 5 rows
+# print(df_teams.isnull().sum()) # missing values
+
+#print(df_teams[['TEAM_ID','NICKNAME', 'CITY']].head(10))
