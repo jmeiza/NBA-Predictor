ed7ab17e8e285a6a630a306818ef12940711a51f
johnmoses <jmoses1519@gmail.com>
2026-06-26 16:02:40 -0400

    Changing the data the model uses to predict the winner of each game. Led to an improvement of around 10% in the accuracy of the model.

--- feature_engineering.py
+++ feature_engineering.py
@@ -2,29 +2,40 @@ import pandas as pd
 
 df = pd.read_csv("games.csv")
 df_teams = pd.read_csv("teams.csv")
+df_ranking = pd.read_csv("ranking.csv")
+
 
 # Adding a column to record when the team that played away wins. Must come before dropna()
 df['AWAY_TEAM_WINS'] = 1 - df['HOME_TEAM_WINS']
 
 df = df.dropna() # dropping missing rows
 
-# getting the average stats for each time when they were at home. We reset the index because after using groupby(), HOME_TEAM_ID becomes an index which can't be used in a merge.
-home_stats=df.groupby('HOME_TEAM_ID')[['PTS_home','FG_PCT_home','AST_home','REB_home','HOME_TEAM_WINS']].mean().reset_index()
+# Dropping seasons that are before the 2020 season
+df = df[df['SEASON'] >= 2020]
+
+# NOTE: "left_on" refers to the DataFrame you are merging from (df) and "right_on" refers to the DataFrame you're merging in (df_ranking). It merges whenever the value of the columns in the lists match,
+
+# Merging the stats for the home team in each game
+home_stats = df.merge(df_ranking[['TEAM_ID', 'STANDINGSDATE', 'W_PCT']], left_on=['TEAM_ID_home', 'GAME_DATE_EST'], right_on=['TEAM_ID', 'STANDINGSDATE'])
+
+# Renaming columns to improve clarity
+home_stats = home_stats.rename(columns={
+    'W_PCT': 'W_PCT_home',
+    'TEAM_ID': 'TEAM_ID_home_rank'})
+
+# Merging the stats for the away team for each game
+complete_stats = home_stats.merge(df_ranking[['TEAM_ID', 'STANDINGSDATE', 'W_PCT']], left_on=['VISITOR_TEAM_ID', 'GAME_DATE_EST'], right_on=['TEAM_ID', 'STANDINGSDATE'])
+
+# Renaming columns to improve clarity
+complete_stats = complete_stats.rename(columns={
+    'W_PCT': 'W_PCT_away',
+    'VISITOR_TEAM_ID': 'AWAY_TEAM_ID'
+})
+
+# Dropping redundant columns
+complete_stats = complete_stats.drop(columns=['TEAM_ID_home_rank', 'STANDINGSDATE_x', 'STANDINGSDATE_y', 'TEAM_ID', 'TEAM_ID_home', 'TEAM_ID_away'])
 
 
-# getting the average stats for each time a team is playing away
-away_stats = df.groupby('VISITOR_TEAM_ID')[['PTS_away','FG_PCT_away','AST_away','REB_away','AWAY_TEAM_WINS']].mean().reset_index()
-
-
-# NOTE: "left_on" refers to the DataFrame you are merging from (home_stats) and "right_on" refers to the DataFrame you're merging in (df_teams). It merges whenever home_Team_id and team_id match.
-home_stats = home_stats.merge(df_teams[['TEAM_ID','NICKNAME']], left_on='HOME_TEAM_ID', right_on='TEAM_ID')
-
-# Merging in the away stats
-complete_stats = home_stats.merge(away_stats, left_on='HOME_TEAM_ID', right_on='VISITOR_TEAM_ID')
-
-# Dropping the two columns since they are technically just duplicates of each other
-complete_stats = complete_stats.drop(columns=['HOME_TEAM_ID', 'VISITOR_TEAM_ID'])
-
-print(complete_stats.columns)
+# print(complete_stats.columns)
 
 complete_stats.to_csv("team_stats.csv", index=False)
